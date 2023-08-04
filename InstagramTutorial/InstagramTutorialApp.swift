f51505df2ef61b359cd0fa2dd651ce1cf1121f5c
Oleg Panchenko <77533590+Oleg-Panchenko@users.noreply.github.com>
2023-08-05 00:20:40 +0300

    Initial Commit

---
+++ InstagramTutorial/InstagramTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramTutorialApp.swift
+//  InstagramTutorial
+//
+//  Created by Panchenko Oleg on 05.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagramTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
