f9660da8115bd07a1903ed951005a0b49cfee135
Oleg Panchenko <77533590+Oleg-Panchenko@users.noreply.github.com>
2023-08-06 22:54:07 +0300

    login ui

---
+++ InstagramTutorial/Core/Authentication/LoginView.swift
@@ -0,0 +1,120 @@
+//
+//  LoginView.swift
+//  InstagramTutorial
+//
+//  Created by Panchenko Oleg on 06.08.2023.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    var body: some View {
+        NavigationStack {
+            VStack {
+
+                Spacer()
+
+                //logo image
+                Image("instagram")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 220, height: 100)
+
+                //text fields
+                VStack {
+                    TextField("Enter your email", text: $email)
+                        .textInputAutocapitalization(.never)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+
+                    SecureField("Password", text: $password)
+                        .background(Color(.systemGray6))
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                }
+
+                Button {
+                    print("Show forgot password")
+                } label: {
+                    Text("Forgot Password?")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.top)
+                        .padding(.trailing, 28)
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+
+
+                Button {
+                    print("Log in")
+                } label: {
+                    Text("Log in")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .frame(width: 360, height: 44)
+                        .background(Color(.systemBlue))
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+                .padding(.vertical)
+
+                HStack {
+                    Rectangle()
+                        .frame(width: (UIScreen.main.bounds.width / 2) - 40, height: 0.5)
+
+                    Text("OR")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+
+                    Rectangle()
+                        .frame(width: (UIScreen.main.bounds.width / 2) - 40, height: 0.5)
+                }
+                .foregroundColor(.gray)
+
+                HStack {
+                    Image("facebook_logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 25, height: 25)
+
+                    Text("Continue with Facebook")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color(.systemBlue))
+                }
+                .padding(.top, 8)
+
+                Spacer()
+
+                Divider()
+
+                NavigationLink {
+                    Text("Sign up")
+                } label: {
+                    HStack(spacing: 3) {
+                        Text("Don't have an account?")
+
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                    .font(.footnote)
+                }
+                .padding(.vertical, 16)
+            }
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
