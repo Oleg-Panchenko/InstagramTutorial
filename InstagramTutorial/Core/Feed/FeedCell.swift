c1cef37c7267d56631cef81b78d25f72fe7d02a5
Oleg Panchenko <77533590+Oleg-Panchenko@users.noreply.github.com>
2023-08-18 23:25:38 +0300

    fetch and display images from firebase storage

--- InstagramTutorial/Core/Feed/FeedCell.swift
+++ InstagramTutorial/Core/Feed/FeedCell.swift
@@ -15,12 +15,8 @@ struct FeedCell: View {
             // image + username
             HStack {
                 if let user = post.user {
-                    Image(user.profileImageUrl ?? "")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 40, height: 40 )
-                        .clipShape(Circle())
-
+                    CircularProfileImageView(user: user, size: ProfileImageSize.xSmall)
+                    
                     Text(user.username)
                         .font(.footnote)
                         .fontWeight(.semibold)
---
+++ InstagramTutorial/Model/User.swift
@@ -0,0 +1,33 @@
+//
+//  User.swift
+//  InstagramTutorial
+//
+//  Created by Panchenko Oleg on 07.08.2023.
+//
+
+import Foundation
+import Firebase
+
+struct User: Identifiable, Codable, Hashable {
+    let id: String
+    var username: String
+    var profileImageUrl: String?
+    var fullName: String?
+    var bio: String?
+    let email: String
+
+    var isCurrentUser: Bool {
+        guard let currentUid = Auth.auth().currentUser?.uid else { return false }
+        return currentUid == id
+    }
+}
+
+extension User {
+    static var MOCK_USERS: [User] = [
+        .init(id: NSUUID().uuidString, username: "Iron Man", profileImageUrl: nil, fullName: "Tony Stark", bio: "Avenger", email: "tony@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "Doctor Strange", profileImageUrl: nil, fullName: "Stephen", bio: "Avenger", email: "doctorStrange@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "Iron Man", profileImageUrl: nil, fullName: "Tony Stark", bio: "Biilionare and just a good man", email: "tony@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "Doctor Strange", profileImageUrl: nil, fullName: "Stephen", bio: "Avenger", email: "doctorStrange@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "Iron Man", profileImageUrl: nil, fullName: "Tony Stark", bio: "Avenger", email: "tony@gmail.com")
+    ]
+}
